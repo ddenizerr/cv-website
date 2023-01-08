01655e858280ef3231b774d99f4a4cef72e39814
ddenizerr <ddenizer14@gmail.com>
2023-01-08 21:39:10 +0000

    - setup of css file finished

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+                'resources/css/app.css',
+                'resources/css/custom.css',
             ],
             refresh: true,
         }),
